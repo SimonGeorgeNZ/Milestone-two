c622efa2420e4b15f4eee4651e62ebb006991b58
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2020-04-30 01:28:41 +0000

    successfully locating player data within API data arrays to use

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,31 @@
-var xhr = new XMLHttpRequest()
+const baseURL = "https://cors-anywhere.herokuapp.com/https://api.sportradar.us/rugby/trial/v2/union/en/teams/sr:competitor:4227/profile.json?api_key=7h8xjjyjyg7dytdr6pdp76kn";
 
-xhr.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest()
 
-xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.sportradar.us/rugby/trial/v2/union/en/teams/sr:competitor:4227/profile.json?api_key=7h8xjjyjyg7dytdr6pdp76kn", true);
+    xhr.open("GET", baseURL)
+    xhr.send();
 
-xhr.send();
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+function writeToDocument(type) {
+    getData(type, function (data) {
+        data = data.players;
+
+        data.forEach(function (item) {
+            document.getElementById("data").innerHTML += "<p>" + item.name + 
+            "<br>" + item.type +
+            "<br>" + item.height +
+            "<br>" + item.weight + "</p>";
+        })
+    });
+}
+
+
+
+
